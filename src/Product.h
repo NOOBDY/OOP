c8d39b32733fb6009640bc11a3e3d828cd20d0c5
noobdy <htommyf12@gmail.com>
2022-11-11 11:27:52 +0800

    mid term

---
+++ src/Product.h
@@ -0,0 +1,34 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+class Product {
+public:
+    Product() = default;
+    Product(std::string name, int price);
+
+    Product &operator=(const Product &other) = default;
+    bool operator==(const Product &other) const;
+
+    std::string getName() const { return m_Name; }
+    int getPrice() const { return m_Price; }
+
+private:
+    std::string m_Name;
+    int m_Price;
+};
+
+Product::Product(std::string name, int price) {
+    if (name.size() < 4 || price < 0)
+        throw std::string("Invalid Value");
+
+    m_Name = name;
+    m_Price = price;
+}
+
+bool Product::operator==(const Product &other) const {
+    return m_Name == other.m_Name;
+}
+
+#endif
--- test/ut_main.cpp
+++ test/ut_main.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "ut_drink.h"
-#include "ut_topping.h"
+#include "ut_sample.h"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
